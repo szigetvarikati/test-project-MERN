6f922b765383d62ff7a10393031e74475435f091
szigetvarikati <szigetvarikati@gmail.com>
2023-10-30 00:59:45 +0100

    implement data fetch from the backend

--- frontend/src/screens/ProductTable.js
+++ frontend/src/screens/ProductTable.js
@@ -1,6 +1,38 @@
-import data from '../data';
+import { useEffect, useReducer } from 'react';
+import axios from 'axios';
+import logger from 'use-reducer-logger';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, products: action.payload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 
 function ProductTable() {
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    products: [],
+    loading: true,
+    error: '',
+  });
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const result = await axios.get('/api/products');
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <div>
       <h1>Termékeink</h1>
@@ -10,19 +42,25 @@ function ProductTable() {
             <tr>
               <th>Cikkszám</th>
               <th>Cikk megnevezése</th>
-              <th>Nettó ár</th>
-              <th>Áfa</th>
+              <th>Nettó ár (Ft)</th>
+              <th>Áfa (%)</th>
             </tr>
           </thead>
           <tbody>
-            {data.products.map((product) => (
-              <tr className="product" key={product._id}>
-                <td>{product.number}</td>
-                <td>{product.name}</td>
-                <td>{product.price}</td>
-                <td>{product.vat}</td>
-              </tr>
-            ))}
+            {loading ? (
+              <div>Loading...</div>
+            ) : error ? (
+              <div>{error}</div>
+            ) : (
+              products.map((product) => (
+                <tr className="product" key={product._id}>
+                  <td>{product.number}</td>
+                  <td>{product.name}</td>
+                  <td>{product.price}</td>
+                  <td>{product.vat}</td>
+                </tr>
+              ))
+            )}
           </tbody>
         </table>
       </div>
