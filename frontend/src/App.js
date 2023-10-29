a4debd4b0394ae39396903fc274a3bac31160f8f
szigetvarikati <szigetvarikati@gmail.com>
2023-10-29 22:21:50 +0100

    implement the product table with basic css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,5 @@
+import data from './data';
+
 function App() {
   return (
     <div>
@@ -6,7 +8,31 @@ function App() {
           Netlient Kft. - Teszt feladat - Készítette: Szigetvári Katalin
         </a>
       </header>
-      <main>Itt lesz a terméklista</main>
+      <main>
+        <h1>Termékeink</h1>
+        <div>
+          <table className="product-table">
+            <thead>
+              <tr>
+                <th>Cikkszám</th>
+                <th>Cikk megnevezése</th>
+                <th>Nettó ár</th>
+                <th>Áfa</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.products.map((product) => (
+                <tr className="product" key={product._id}>
+                  <td>{product.number}</td>
+                  <td>{product.name}</td>
+                  <td>{product.price}</td>
+                  <td>{product.vat}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </main>
     </div>
   );
 }
