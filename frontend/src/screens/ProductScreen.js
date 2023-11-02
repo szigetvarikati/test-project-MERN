ac1a7144eabf84197a194836087196756027fcbe
szigetvarikati <szigetvarikati@gmail.com>
2023-11-02 15:01:10 +0100

    create a ProductTable component and use it

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,11 @@
+import ProductTable from '../components/ProductTable';
+
+function ProductScreen() {
+  return (
+    <div>
+      <ProductTable />
+    </div>
+  );
+}
+
+export default ProductScreen;
