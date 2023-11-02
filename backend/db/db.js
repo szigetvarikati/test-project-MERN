5e31bbfb6f3b8339e2cbf41fded11f2c7b0ce134
szigetvarikati <szigetvarikati@gmail.com>
2023-11-02 13:30:14 +0100

    refactor connectToDatabase functin

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,10 +1,15 @@
 import mongoose from 'mongoose';
 
 export async function connectToDatabase() {
-  return mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 export function closeDatabaseConnection() {
