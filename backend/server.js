1d847a232871fcc03af0260f275b8eff87960f7a
szigetvarikati <szigetvarikati@gmail.com>
2023-11-02 13:30:45 +0100

    use connestToDatabase() in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,12 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
 import userRouter from './routes/userRoutes.js';
+import { connectToDatabase } from './db/db.js';
 
 dotenv.config();
-
-mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+connectToDatabase();
 
 const app = express();
 
@@ -27,7 +16,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
-
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
