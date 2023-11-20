b32b3945efbfdef6262dab70100676aec3a6113e
szigetvarikati <szigetvarikati@gmail.com>
2023-11-20 10:25:30 +0100

    modofy the userModel (add firstname, lastname, isAdnim fields)

--- backend/db/models/userModel.js
+++ backend/db/models/userModel.js
@@ -2,9 +2,12 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema(
   {
+    firstname: { type: String, required: false },
+    lastname: { type: String, required: false },
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    isAdmin: { type: Boolean, required: true },
   },
   { versionKey: false, timestamps: true }
 );
